98978a094e8fd3b859d77cd7c4feedda9cffaaa3
Alex <alexstephens9@gmail.com>
2020-10-16 15:37:53 +0100

    Fixed bugs with displayed sigmas, added module for data input

---
+++ data.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+
+import dateutil.parser as dp
+
+
+def loadData():
+
+    data_url = 'http://www.robots.ox.ac.uk/~mosb/teaching/AIMS_CDT/sotonmet.txt'
+    data = pd.read_csv(data_url)
+
+    print(data.columns)
+
+    # Time in days since first reading
+    t = data['Reading Date and Time (ISO)']
+    t = [int(dp.parse(x).strftime('%s')) for x in t]
+    t = np.array([float(x - t[0])/86400 for x in t]).reshape(-1,1)
+
+    # Training and test data
+    y = data['Tide height (m)']
+    ytrue = data['True tide height (m)']
+
+    # Remove missing data points
+    data_train = [(t[i], y[i]) for i in range(len(t)) if not np.isnan(y[i])]
+    data_test = [(t[i], ytrue[i]) for i in range(len(t)) if not np.isnan(ytrue[i])]
+    t, y = [x[0] for x in data_train], [x[1] for x in data_train]
+    ttrue, ytrue = np.array([x[0] for x in data_test]).reshape(-1,1), [x[1] for x in data_test]
+
+    # Normalise both datasetts
+    mean, stdev = np.mean(y), np.std(y)
+    y = np.array([(yi - mean) / stdev for yi in y]).reshape(-1,1)
+    ytrue = np.array([(yi - mean) / stdev for yi in ytrue]).reshape(-1,1)
+
+    return t, y, ttrue, ytrue
--- vis.py
+++ vis.py
@@ -9,19 +9,22 @@ def getFunctionSample(mu, sigma):
 def plotTides(Xs, mu, sigma, t, y, ttrue, ytrue):
 
     # Plot width and height in inches
-    width, height = 12,6
+    width, height = 10, 5
     pylab.rcParams['figure.figsize'] = width, height
+    pylab.rcParams['figure.dpi'] = 100 # for export
+    pylab.rcParams.update({'font.size': 22})
+    pylab.rcParams.update({'legend.fontsize': 12})
 
     # Space above plot
     print('\n\n')
 
     # Uncertainties (sigma and 2sigma)
-    y1 = np.array([mu[i] + 2 * sigma[i][i] for i in range(len(Xs))]).ravel()
-    y2 = np.array([mu[i] - 2 * sigma[i][i] for i in range(len(Xs))]).ravel()
-    pylab.fill_between(Xs.ravel(), y1, y2, linewidth=0, color='orange', alpha=0.2, label='2 SD')
-    y1 = np.array([mu[i] + 1 * sigma[i][i] for i in range(len(Xs))]).ravel()
-    y2 = np.array([mu[i] - 1 * sigma[i][i] for i in range(len(Xs))]).ravel()
+    y1 = np.array([mu[i] + 1 * np.sqrt(sigma[i][i]) for i in range(len(Xs))]).ravel()
+    y2 = np.array([mu[i] - 1 * np.sqrt(sigma[i][i]) for i in range(len(Xs))]).ravel()
     pylab.fill_between(Xs.ravel(), y1, y2, linewidth=0, color='orange', alpha=0.5, label='1 SD')
+    y1 = np.array([mu[i] + 2 * np.sqrt(sigma[i][i]) for i in range(len(Xs))]).ravel()
+    y2 = np.array([mu[i] - 2 * np.sqrt(sigma[i][i]) for i in range(len(Xs))]).ravel()
+    pylab.fill_between(Xs.ravel(), y1, y2, linewidth=0, color='orange', alpha=0.2, label='2 SD')
 
     # measurements and truth data
     pylab.plot(t, y, '.r', label='training data')
@@ -36,9 +39,10 @@ def plotTides(Xs, mu, sigma, t, y, ttrue, ytrue):
         fs = getFunctionSample(mu, sigma)
         pylab.plot(Xs, fs, color='green', alpha=0.3, label='function draws')
 
-    pylab.legend(loc='upper right')
+    pylab.legend(loc='upper center', ncol=5)
     pylab.xlabel('time (days)')
     pylab.ylabel('tide height (normalised)')
-    pylab.ylim((-5, 5))
+    yl = 3
+    pylab.ylim((-yl, yl))
 
     pylab.show()
